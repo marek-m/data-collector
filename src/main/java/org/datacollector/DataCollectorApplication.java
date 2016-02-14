9eef1024d3f7297fce12353113588311c748bdc5
marek-m <marek_mat@poczta.onet.pl>
2016-02-14 19:04:37 +0100

    Init spring boot project

---
+++ src/main/java/org/datacollector/DataCollectorApplication.java
@@ -0,0 +1,12 @@
+package org.datacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectorApplication.class, args);
+	}
+}
---
+++ src/test/java/org/datacollector/DataCollectorApplicationTests.java
@@ -0,0 +1,18 @@
+package org.datacollector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DataCollectorApplication.class)
+@WebAppConfiguration
+public class DataCollectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
