ae7eb8dd00dc9cd6231d47d49271d9e97a0d7893
marek-m <marek_mat@poczta.onet.pl>
2016-02-14 21:35:07 +0100

    simple rest controller

--- src/main/java/org/datacollector/DataCollectorApplication.java
+++ src/main/java/org/datacollector/DataCollectorApplication.java
@@ -1,9 +1,11 @@
 package org.datacollector;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan(basePackages = { "org.datacollector" })
+@EnableAutoConfiguration
 public class DataCollectorApplication {
 
 	public static void main(String[] args) {
