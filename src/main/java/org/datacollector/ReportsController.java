768ca756ea98aa2de9d4beb737f9bc5db7bc8691
marek-m <marek_mat@poczta.onet.pl>
2016-02-25 22:01:23 +0100

    response body

--- src/main/java/org/datacollector/ReportsController.java
+++ src/main/java/org/datacollector/ReportsController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.datacollector.db.model.ReportModel;
 import org.datacollector.service.ReportService;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,8 +31,10 @@ public class ReportsController {
 	}
 	
 	@RequestMapping("/getAll")
+	@ResponseBody
 	public List<ReportModel> getAll() {
 		try {
+			
 			return service.getReports();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
