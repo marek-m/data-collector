c423e9286fe5b9a2e3ac2167c8bbf0ed8fb91db3
marek-m <marek_mat@poczta.onet.pl>
2016-02-25 22:18:16 +0100

    cross-origin for controller

--- src/main/java/org/datacollector/ReportsController.java
+++ src/main/java/org/datacollector/ReportsController.java
@@ -6,11 +6,13 @@ import org.datacollector.db.model.ReportModel;
 import org.datacollector.service.ReportService;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/reports")
 public class ReportsController {
