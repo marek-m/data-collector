a1d1a763d06fe1f44b4c9ca800dbdfe4581bfa38
marek-m <marek_mat@poczta.onet.pl>
2016-03-26 12:45:29 +0100

    spring-security test

--- src/main/java/org/datacollector/security/SecurityConfig.java
+++ src/main/java/org/datacollector/security/SecurityConfig.java
@@ -2,11 +2,26 @@ package org.datacollector.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/**").permitAll() 
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()                                    
+                .permitAll();
+    }
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
