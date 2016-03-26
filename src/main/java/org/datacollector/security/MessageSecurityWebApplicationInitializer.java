94fc224f1eec0cec910e8b70410612ed770edeef
marek-m <marek_mat@poczta.onet.pl>
2016-03-26 12:32:21 +0100

    spring-security test

---
+++ src/main/java/org/datacollector/security/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package org.datacollector.security;
+
+import org.springframework.security.web.context.*;
+
+public class MessageSecurityWebApplicationInitializer
+      extends AbstractSecurityWebApplicationInitializer {
+}
