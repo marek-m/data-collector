2c3709b542aa6922e66baa6d27ae2c6fdaecae38
marek-m <marek_mat@poczta.onet.pl>
2016-03-28 21:48:16 +0200

    Added thymeleaf test

--- src/main/java/org/datacollector/HelloController.java
+++ src/main/java/org/datacollector/HelloController.java
@@ -1,5 +1,6 @@
 package org.datacollector;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,11 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String hello(@RequestParam String name) {
 		return "Hello "+name;
+	}
+	
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
 	}
 }
