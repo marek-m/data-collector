0bf2c30655e99984c1598fe95246996cbc3299c3
marek-m <marek_mat@poczta.onet.pl>
2016-04-24 11:34:52 +0200

    Redirect to index after login

--- src/main/java/org/datacollector/security/SecurityConfig.java
+++ src/main/java/org/datacollector/security/SecurityConfig.java
@@ -1,17 +1,29 @@
 package org.datacollector.security;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsService userDetailsService;
+    
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/page/login")
+                .successHandler(new AuthenticationSuccessHandler() {
+					@Override
+					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+						redirectStrategy.sendRedirect(request, response, "page/index");
+					}
+				})
                 .permitAll();
     }
     @Autowired
