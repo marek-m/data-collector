a34d5171d87f486b9e6c57637059056b9c75d01d
marek-m <marek_mat@poczta.onet.pl>
2016-04-24 23:08:39 +0200

    ...

--- src/main/java/org/datacollector/ReportsController.java
+++ src/main/java/org/datacollector/ReportsController.java
@@ -49,7 +49,7 @@ public class ReportsController {
 			throw new Exception("Type parameter must be in range between 0 and 5");
 		}
 		service.addReport(report, getPrincipal());
-		return "redirect:/page/login";
+		return "redirect:/page/about";
 	}
 		
 	@RequestMapping("/getAll")
